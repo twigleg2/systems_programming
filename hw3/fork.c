0fab92885cb51cfd876e9015bdbaff82fe91e8df
Gaven Finch <gavenfinch@gmail.com>
2019-09-20 19:11:07 -0600

    Finish part 5

--- hw3/fork.c
+++ hw3/fork.c
@@ -8,7 +8,18 @@ int main(int argc, char *argv[])
 {
 	int pid;
 
-	printf("Starting program; process has pid %d\n", getpid());
+	// printf("Starting program; process has pid %d\n", getpid());
+
+	FILE *file = fopen("fork-output.txt", "w");
+	fprintf(file, "BEFORE FORK\n");
+	fflush(file);
+
+	int p[2];
+	if (pipe(p) < 0)
+	{
+		puts("bad pipe");
+		exit(0);
+	}
 
 	if ((pid = fork()) < 0)
 	{
@@ -17,19 +28,26 @@ int main(int argc, char *argv[])
 	}
 
 	/* BEGIN SECTION A */
+	fprintf(file, "SECTION A\n");
+	fflush(file);
 
-	printf("Section A;  pid %d\n", getpid());
-	sleep(30);
+	// printf("Section A;  pid %d\n", getpid());
+	// sleep(30);
 
 	/* END SECTION A */
 	if (pid == 0)
 	{
 		/* BEGIN SECTION B */
+		fprintf(file, "SECTION B\n");
+		fflush(file);
 
-		printf("Section B\n");
-		sleep(30);
-		sleep(30);
-		printf("Section B done sleeping\n");
+		close(p[0]);
+		FILE *write_pipe = fdopen(p[1], "w");
+		fputs("Hello from section B\n", write_pipe);
+
+		// printf("Section B\n");
+		// sleep(30);
+		// printf("Section B done sleeping\n");
 
 		exit(0);
 
@@ -38,20 +56,31 @@ int main(int argc, char *argv[])
 	else
 	{
 		/* BEGIN SECTION C */
+		fprintf(file, "SECTION C\n");
+		fflush(file);
 
-		printf("Section C\n");
-		sleep(30);
-		// wait(NULL);
-		printf("Section C done sleeping\n");
+		close(p[1]);
+		FILE *read_pipe = fdopen(p[0], "r");
+		char message[100];
+		fgets(message, 100, read_pipe);
+		printf("%s\n", message);
+
+		// printf("Section C\n");
+		// sleep(30);
+		wait(NULL);
+		// sleep(30);
+		// printf("Section C done sleeping\n");
 
 		exit(0);
 
 		/* END SECTION C */
 	}
 	/* BEGIN SECTION D */
+	fprintf(file, "SECTION D\n");
+	fflush(file);
 
 	printf("Section D\n");
-	sleep(30);
+	// sleep(30);
 
 	/* END SECTION D */
 }
