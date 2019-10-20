a8fde54e4a156c377335454b30c3a4e95c1dd016
Gaven Finch <gavenfinch@gmail.com>
2019-10-19 20:16:06 -0600

    Finish part2

--- hw5/client.c
+++ hw5/client.c
@@ -8,7 +8,8 @@
 
 #define BUF_SIZE 500
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	struct addrinfo hints;
 	struct addrinfo *result, *rp;
 	int sfd, s, j;
@@ -16,7 +17,8 @@ int main(int argc, char *argv[]) {
 	ssize_t nread;
 	char buf[BUF_SIZE];
 
-	if (argc < 3) {
+	if (argc < 3)
+	{
 		fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
@@ -24,13 +26,15 @@ int main(int argc, char *argv[]) {
 	/* Obtain address(es) matching host/port */
 
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6 */
+	// hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_socktype = SOCK_STREAM; /* TCP socket */
 	hints.ai_flags = 0;
-	hints.ai_protocol = 0;          /* Any protocol */
+	hints.ai_protocol = 0; /* Any protocol */
 
 	s = getaddrinfo(argv[1], argv[2], &hints, &result);
-	if (s != 0) {
+	if (s != 0)
+	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
@@ -40,53 +44,58 @@ int main(int argc, char *argv[]) {
 	   If socket(2) (or connect(2)) fails, we (close the socket
 	   and) try the next address. */
 
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
+	for (rp = result; rp != NULL; rp = rp->ai_next)
+	{
 		sfd = socket(rp->ai_family, rp->ai_socktype,
-				rp->ai_protocol);
+					 rp->ai_protocol);
 		if (sfd == -1)
 			continue;
 
 		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-			break;                  /* Success */
+			break; /* Success */
 
 		close(sfd);
 	}
 
-	if (rp == NULL) {               /* No address succeeded */
+	if (rp == NULL)
+	{ /* No address succeeded */
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
-
 	}
 
-	freeaddrinfo(result);           /* No longer needed */
+	freeaddrinfo(result); /* No longer needed */
 
 	/* Send remaining command-line arguments as separate
 	   datagrams, and read responses from server */
 
-	for (j = 3; j < argc; j++) {
+	for (j = 3; j < argc; j++)
+	{
 		len = strlen(argv[j]) + 1;
 		/* +1 for terminating null byte */
 
-		if (len + 1 > BUF_SIZE) {
+		if (len + 1 > BUF_SIZE)
+		{
 			fprintf(stderr,
 					"Ignoring long message in argument %d\n", j);
 			continue;
 		}
 
-		if (write(sfd, argv[j], len) != len) {
+		sleep(2);
+		if (write(sfd, argv[j], len) != len)
+		{
 			fprintf(stderr, "partial/failed write\n");
 			exit(EXIT_FAILURE);
 		}
 
-		nread = read(sfd, buf, BUF_SIZE);
-		if (nread == -1) {
-			perror("read");
-			exit(EXIT_FAILURE);
-		}
+		// nread = read(sfd, buf, BUF_SIZE);
+		// if (nread == -1)
+		// {
+		// 	perror("read");
+		// 	exit(EXIT_FAILURE);
+		// }
 
 		printf("Received %zd bytes: %s\n", nread, buf);
 	}
 
 	exit(EXIT_SUCCESS);
 }
-
