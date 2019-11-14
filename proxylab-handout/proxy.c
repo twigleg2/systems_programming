ed2ad28b940ad8a1a70db1d4117d0d2436937394
Gaven Finch <gavenfinch@gmail.com>
2019-11-14 15:05:58 -0700

    Finish send a receive a response, not rubust
    Fix tiny bug

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -18,8 +18,6 @@ static const char *user_agent_hdr =
 
 int main(int argc, char *argv[])
 {
-    char buf[MAX_OBJECT_SIZE];
-
     if (argc < 2)
     {
         printf("usage: %s port\n", argv[0]);
@@ -49,148 +47,185 @@ int main(int argc, char *argv[])
         perror("listen error");
         exit(EXIT_FAILURE);
     }
-    // my server code
-    struct sockaddr_storage peer_addr;
-    socklen_t peer_addr_len;
-    ssize_t nread;
-    int sfd2 = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_len);
-    nread = read(sfd2, buf, MAX_OBJECT_SIZE);       // TODO: read in a loop?
-    printf("got %d bytes:\n%s\n", (int)nread, buf); // TODO: potentially won't output all the data received.
+    //LOOP
+    while (1)
+    {
+        // my server code
+        struct sockaddr_storage peer_addr;
+        socklen_t peer_addr_len;
+        char buf[MAX_OBJECT_SIZE];
+        ssize_t nread;
+        int sfd2 = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_len);
+        nread = read(sfd2, buf, MAX_OBJECT_SIZE);       // TODO: read in a loop?
+        printf("got %d bytes:\n%s\n", (int)nread, buf); // TODO: potentially won't output all the data received.
 
-    //begin parsing
-    char *req_type = strtok(buf, " ");
-    if (strcmp(req_type, "GET") != 0)
-    {
-        printf("Bad req_type: %s\n", req_type);
-        // TODO: discard request
-    }
-    char *http = strtok(NULL, "//");
-    if (strcmp(http, "http:") != 0)
-    {
-        printf("bad http: %s\n", http);
-        // TODO: discard request
-    }
-    char *host = strtok(NULL, "/");
-    if (strlen(host) == 0)
-    {
-        printf("bad host: %s\n", host);
-        // TODO: discard request
-    }
-    // find if contains port
-    char *port = NULL; //default
-    char *colonPos = strchr(host, ':');
-    if (colonPos)
-    {
-        port = colonPos + 1; //set port to point to one char after colon
-        colonPos[0] = '\0';  //change colon to null char
-    }
-    printf("host: %s, port: %s\n", host, port);
-    char *path = strtok(NULL, "\r\n"); //path contains the rest of the first line, including HTTP/1.1
-    char *pathEnd = strchr(path, ' '); //find the first space, which is the real end of the path.
-    pathEnd[0] = '\0';
-    if (path == NULL)
-    {
-        printf("bad request, not HTTP/1.1");
-        // TODO discard request;
-    }
-    printf("path: %s\n\n", path);
-    // TODO: verify that HTTP/1.1 was sent, else invalid request
-
-    // begin putting together my request
-    char myRequest[MAX_OBJECT_SIZE] = "";
-    strcat(myRequest, req_type);
-    strcat(myRequest, " /"); //path is missing the beggining slash, so adding it here.
-    strcat(myRequest, path);
-    strcat(myRequest, " ");
-    strcat(myRequest, "HTTP/1.0\r\n");
-
-    int hasHostHeader = 0;
-    char *header;
-    while ((header = strtok(NULL, "\r\n")) != NULL)
-    {
-        char *colon = strchr(header, ':');
-        colon[0] = '\0'; // temporary
-
-        if (strcmp(header, "Connection") == 0)
+        //begin parsing
+        char *req_type = strtok(buf, " ");
+        if (strcmp(req_type, "GET") != 0)
         {
-            //throw away, I'll add my own later
-            continue;
+            printf("Bad req_type: %s\n", req_type);
+            // TODO: discard request
         }
-        if (strcmp(header, "Proxy-Connection") == 0)
+        char *http = strtok(NULL, "//");
+        if (strcmp(http, "http:") != 0)
         {
-            //throw away, I'll add my own later
-            continue;
+            printf("bad http: %s\n", http);
+            // TODO: discard request
         }
-        if (strcmp(header, "User-Agent") == 0)
+        char *host = strtok(NULL, "/");
+        if (strlen(host) == 0)
         {
-            //throw away, I'll add my own later
-            continue;
+            printf("bad host: %s\n", host);
+            // TODO: discard request
         }
-        if (strcmp(header, "Host") == 0)
+        // find if contains port
+        char *port = NULL; //default
+        char *colonPos = strchr(host, ':');
+        if (colonPos)
         {
-            hasHostHeader = 1;
+            port = colonPos + 1; //set port to point to one char after colon
+            colonPos[0] = '\0';  //change colon to null char
         }
+        printf("host: %s, port: %s\n", host, port);
+        char *path = strtok(NULL, "\r\n"); //path contains the rest of the first line, including HTTP/1.1
+        char *pathEnd = strchr(path, ' '); //find the first space, which is the real end of the path.
+        pathEnd[0] = '\0';
+        if (path == NULL)
+        {
+            printf("bad request, not HTTP/1.1");
+            // TODO discard request;
+        }
+        printf("path: %s\n\n", path);
+        // TODO: verify that HTTP/1.1 was sent, else invalid request
 
-        colon[0] = ':';
-        strcat(myRequest, header);
+        // begin putting together my request
+        char myRequest[MAX_OBJECT_SIZE] = "";
+        strcat(myRequest, req_type);
+        strcat(myRequest, " /"); //path is missing the beggining slash, so adding it here.
+        strcat(myRequest, path);
+        strcat(myRequest, " ");
+        strcat(myRequest, "HTTP/1.0\r\n");
+
+        int hasHostHeader = 0;
+        char *header;
+        while ((header = strtok(NULL, "\r\n")) != NULL)
+        {
+            char *colon = strchr(header, ':');
+            colon[0] = '\0'; // temporary
+
+            if (strcmp(header, "Connection") == 0)
+            {
+                //throw away, I'll add my own later
+                continue;
+            }
+            if (strcmp(header, "Proxy-Connection") == 0)
+            {
+                //throw away, I'll add my own later
+                continue;
+            }
+            if (strcmp(header, "User-Agent") == 0)
+            {
+                //throw away, I'll add my own later
+                continue;
+            }
+            if (strcmp(header, "Host") == 0)
+            {
+                hasHostHeader = 1;
+            }
+
+            colon[0] = ':';
+            strcat(myRequest, header);
+            strcat(myRequest, "\r\n");
+        }
+        if (!hasHostHeader)
+        {
+            strcat(myRequest, host);
+            strcat(myRequest, "\r\n");
+        }
+        // headers required by the lab
+        strcat(myRequest, user_agent_hdr);
+        strcat(myRequest, "Connection: close");
         strcat(myRequest, "\r\n");
-    }
-    if (!hasHostHeader)
-    {
-        strcat(myRequest, host);
+        strcat(myRequest, "Proxy-Connection: close");
         strcat(myRequest, "\r\n");
+
+        strcat(myRequest, "\r\n");
+        printf("myRequest:\n%s\n", myRequest);
+
+        // Provided client code
+        struct addrinfo hints;
+        struct addrinfo *result;
+        struct addrinfo *rp;
+        int sfd3;
+        /* Obtain address(es) matching host/port */
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = SOCK_STREAM; /* TCP socket */
+        hints.ai_flags = 0;
+        hints.ai_protocol = 0;                                          /* Any protocol */
+        int s = getaddrinfo(host, port ? port : "80", &hints, &result); //host and port come from the original http GET request
+        if (s != 0)
+        {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+            exit(EXIT_FAILURE);
+        }
+        /* getaddrinfo() returns a list of address structures.
+    Try each address until we successfully connect(2).
+    If socket(2) (or connect(2)) fails, we (close the socket
+    and) try the next address. */
+        for (rp = result; rp != NULL; rp = rp->ai_next)
+        {
+            sfd3 = socket(rp->ai_family, rp->ai_socktype,
+                          rp->ai_protocol);
+            if (sfd3 == -1)
+                continue;
+            if (connect(sfd3, rp->ai_addr, rp->ai_addrlen) != -1)
+                break; /* Success */
+            close(sfd3);
+        }
+        if (rp == NULL)
+        { /* No address succeeded */
+            fprintf(stderr, "Could not connect\n");
+        }
+        freeaddrinfo(result); /* No longer needed */
+
+        //write
+        int myRequestLen = strlen(myRequest);
+        int bytesWritten = 0;
+        while (bytesWritten != myRequestLen)
+        {
+            int checkErr = write(sfd3, myRequest + bytesWritten, myRequestLen - bytesWritten);
+            if (checkErr == -1)
+            {
+                fprintf(stderr, "write error");
+                exit(EXIT_FAILURE);
+            }
+            bytesWritten += checkErr;
+        }
+
+        char content[MAX_OBJECT_SIZE];
+        read(sfd3, content, MAX_OBJECT_SIZE); // TODO: robust read?
+        printf("content: %s\n", content);
+
+        int contentLen = strlen(content);
+        bytesWritten = 0;
+        while (bytesWritten != contentLen)
+        {
+            int checkErr = write(sfd2, content + bytesWritten, contentLen - bytesWritten);
+            if (checkErr == -1)
+            {
+                fprintf(stderr, "write error");
+                exit(EXIT_FAILURE);
+            }
+            bytesWritten += checkErr;
+            printf("bytesWritten: %d  contentLen: %d\n", bytesWritten, contentLen);
+        }
+        close(sfd3);
+        close(sfd2);
+        printf("end of loop");
     }
-    // headers required by the lab
-    strcat(myRequest, "User-Agent: ");
-    strcat(myRequest, user_agent_hdr);
-    strcat(myRequest, "Connection: close");
-    strcat(myRequest, "\r\n");
-    strcat(myRequest, "Proxy-Connection: close");
-    strcat(myRequest, "\r\n");
+    //END LOOP
 
-    strcat(myRequest, "\r\n");
-    printf("myRequest:\n%s\n", myRequest);
-
-    // // Provided client code
-    // char *host;
-    // char *port;
-    // struct addrinfo hints;
-    // struct addrinfo *result;
-    // struct addrinfo *rp;
-    // /* Obtain address(es) matching host/port */
-    // memset(&hints, 0, sizeof(struct addrinfo));
-    // hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
-    // hints.ai_socktype = SOCK_STREAM; /* TCP socket */
-    // hints.ai_flags = 0;
-    // hints.ai_protocol = 0;                            /* Any protocol */
-    // int s = getaddrinfo(host, port, &hints, &result); //host and port come from the original http GET request
-    // if (s != 0)
-    // {
-    //     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-    //     exit(EXIT_FAILURE);
-    // }
-    // /* getaddrinfo() returns a list of address structures.
-    // Try each address until we successfully connect(2).
-    // If socket(2) (or connect(2)) fails, we (close the socket
-    // and) try the next address. */
-    // for (rp = result; rp != NULL; rp = rp->ai_next)
-    // {
-    //     sfd = socket(rp->ai_family, rp->ai_socktype,
-    //                  rp->ai_protocol);
-    //     if (sfd == -1)
-    //         continue;
-    //     if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-    //         break; /* Success */
-    //     close(sfd);
-    // }
-    // if (rp == NULL)
-    // { /* No address succeeded */
-    //     fprintf(stderr, "Could not connect\n");
-    // }
-    // freeaddrinfo(result); /* No longer needed */
-
-    // //
-
-    printf("%s", user_agent_hdr);
+    // printf("%s", user_agent_hdr);
     return 0;
 }
